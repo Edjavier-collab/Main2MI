6b90c813bd2513bc1aff9a32b1263adc9698718d
Javi <eduardojavierjr@gmail.com>
2025-12-28 16:16:37 -0800

    chore: purge build cache and secure gitignore

--- .next/dev/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/javi/dev/Main2MI/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/3be83a346553616c-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
